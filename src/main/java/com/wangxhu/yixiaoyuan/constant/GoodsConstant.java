b8bca107371cc3fd43fa9417395a52976496a042
StormWangxhu <StormWangxhu@163.com>
2019-04-26 18:45:32 +0800

    创建订单分类编号

--- src/main/java/com/wangxhu/yixiaoyuan/constant/GoodsConstant.java
+++ src/main/java/com/wangxhu/yixiaoyuan/constant/GoodsConstant.java
@@ -10,4 +10,33 @@ public class GoodsConstant {
 
     public static final String GOODS_CONTROLLER_DESC = "处理商品相关的数据畜力请求";
 
+    /**
+     * 分类状态码 数码
+     */
+    public static final int GOODS_SHUMA = 0;
+
+    /**
+     * 学习用品
+     */
+    public static final int GOODS_STUDY_USER = 1;
+
+    /**
+     * 服饰美妆
+     */
+    public static final int GOODS_CLOTHES_BEAUTY = 2;
+
+    /**
+     * 房源信息
+     */
+    public static final int GOODS_HOUSE_INFO = 3;
+
+    /**
+     * 求购信息
+     */
+    public static final int GOODS_WAANT_BUY = 4;
+
+    /**
+     * 其他
+     */
+    public static final int GOODS_OTHERS = 5;
 }
